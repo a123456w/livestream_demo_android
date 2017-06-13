9311a89bca304a5d990a34e9082afece48e9f8a8
wuxiaofei <761241917@qq.com>
2017-06-13 11:24:36 +0800

    编写FragmentDialog类和Adapter类并编写显示礼物图片的方法

---
+++ app/src/main/java/cn/ucai/live/ui/activity/GiftManagementDialog.java
@@ -0,0 +1,115 @@
+package cn.ucai.live.ui.activity;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.DownloadListener;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import cn.ucai.live.LiveHelper;
+import cn.ucai.live.R;
+import cn.ucai.live.data.model.Gift;
+import cn.ucai.live.data.restapi.LiveException;
+import cn.ucai.live.data.restapi.LiveManager;
+import cn.ucai.live.utils.L;
+
+import static android.R.attr.collapseColumns;
+import static android.R.attr.key;
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by wei on 2017/3/3.
+ */
+
+public class GiftManagementDialog extends DialogFragment{
+
+    @BindView(R.id.rv_gift)
+    RecyclerView rvGift;
+    @BindView(R.id.tv_my_bill)
+    TextView tvMyBill;
+    @BindView(R.id.tv_recharge)
+    TextView tvRecharge;
+    Unbinder unbinder;
+    private GridLayoutManager mGridLayoutManager;
+    private LivegiftAdapter adapter;
+    List<Gift> list;
+
+
+
+    public static GiftManagementDialog newInstance() {
+        GiftManagementDialog dialog = new GiftManagementDialog();
+        return dialog;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gift_list, container, false);
+        Window window = getDialog().getWindow();
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        window.getDecorView().setPadding(0, 0, 0, 0);
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        wlp.gravity = Gravity.BOTTOM;
+        wlp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        window.setAttributes(wlp);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+        initView();
+    }
+
+    private void initData() {
+        list = LiveHelper.getInstance().getGiftLists();
+        if(list==null){
+            LiveHelper.getInstance().getGiftList();
+        }
+    }
+
+    private void initView() {
+        mGridLayoutManager = new GridLayoutManager(getContext(), 4);
+        adapter = new LivegiftAdapter(list, getContext());
+        rvGift.setLayoutManager(mGridLayoutManager);
+        rvGift.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
---
+++ app/src/main/java/cn/ucai/live/ui/activity/LivegiftAdapter.java
@@ -0,0 +1,63 @@
+package cn.ucai.live.ui.activity;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.live.R;
+import cn.ucai.live.data.model.Gift;
+
+/**
+ * Created by Administrator on 2017/6/12 0012.
+ */
+
+public class LivegiftAdapter extends RecyclerView.Adapter<LivegiftAdapter.GiftHolder> {
+    List<Gift> list;
+    Context context;
+
+    public LivegiftAdapter(List<Gift> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public GiftHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new GiftHolder(View.inflate(context, R.layout.item_gift, null));
+    }
+
+    @Override
+    public void onBindViewHolder(GiftHolder holder, int position) {
+        Gift gift = list.get(position);
+        holder.tvGiftName.setText(gift.getGname());
+        holder.tvGiftPrice.setText(String.valueOf(gift.getGprice()));
+        EaseUserUtils.setAppAvatar(context,gift.getGurl(),holder.ivGiftThumb);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class GiftHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.ivGiftThumb)
+        ImageView ivGiftThumb;
+        @BindView(R.id.tvGiftName)
+        TextView tvGiftName;
+        @BindView(R.id.tvGiftPrice)
+        TextView tvGiftPrice;
+
+        GiftHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
